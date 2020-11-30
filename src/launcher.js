49c7db4eb675c8d7ce1667900893bf17409c904c
ToxesFoxes <toxes_foxes@outlook.com>
2020-11-30 06:09:18 +0600

    Project structure changed

---
+++ src/launcher.js
@@ -0,0 +1,48 @@
+let isRetentionLoaded = false
+
+function loadRetentionIfNeeded() {
+	if (!isRetentionLoaded) {
+		IMPORT("Retention:2")
+
+		let __version__ = __mod__.getInfoProperty("version")
+		reportError.setTitle(__name__ + " " + __version__)
+		reportError.setInfoMessage("It wasn't possible to fully start modication. " +
+			"If you don't see new interface, please contact developers on our group.")
+
+		isRetentionLoaded = true
+	}
+}
+
+function tryToReport(e) {
+	loadRetentionIfNeeded()
+	reportError(e)
+}
+
+function launchModification(additionalScope) {
+	try {
+		if (additionalScope != undefined) {
+			Launch(additionalScope)
+			return
+		}
+		Launch()
+	} catch (e) {
+		tryToReport(e)
+	}
+}
+
+(function () {
+	try {
+		ConfigureMultiplayer({
+			name: "auto",
+			version: "auto",
+			isClientOnly: true
+		})
+	} catch (e) {
+		launchModification({
+			isOutdated: true
+		})
+		return
+	}
+
+	launchModification()
+})()
